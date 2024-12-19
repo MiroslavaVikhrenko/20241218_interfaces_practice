b55668388c73159f3922b3ae630937ad025f1f7c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-19 14:57:39 -0700

    Practice interfaces in C#

---
+++ 20241219_task8/ClassArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241219_task8
+{
+    /*
+     * Создайте интерфейс «IMath». В нём должно быть четыре метода: 
+
+    ■ int Max() — возвращает максимум; 
+    ■ int Min() — возвращает минимум;
+    ■ float Avg() — возвращает среднеарифметическое; 
+    ■ bool Search(int valueToSearch) — ищет valueSearch внутри контейнера данных.
+    Возвращает true, если значение найдено. Возвращает false, если значение не найдено.
+
+    Класс, созданный в первом задании «Array», должен имплементировать интерфейс «IMath». 
+
+    Метод Max — возвращает максимум среди элементов массива. 
+    Метод Min — возвращает минимум среди элементов массива. 
+    Метод Avg — возвращает среднеарифметическое среди элементов массива. 
+    Метод Search — ищет значение внутри массива. Возвращает true, если значение найдено. Возвращает false, если значение не найдено. Напишите код для тестирования полученной функциональности.
+     */
+    internal class ClassArray : IMath
+    {
+        public string Name { get; set; }
+        public int[] Numbers {  get; set; }
+
+        public int Max()
+        {
+            int max = Numbers[0];
+            int elementNumber = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] > max)
+                {
+                    max = Numbers[i];
+                    elementNumber = i;
+                }
+            }
+            return Numbers[elementNumber];
+        }
+
+        public int Min()
+        {
+            int min = Numbers[0];
+            int elementNumber = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] < min)
+                {
+                    min = Numbers[i];
+                    elementNumber = i;
+                }
+            }
+            return Numbers[elementNumber];
+        }
+
+        public float Avg()
+        {
+            int sum = 0;
+
+            for (int i = 0;i < Numbers.Length;i++)
+            {
+                sum += Numbers[i];
+            }
+
+            return (float)(sum / Numbers.Length);
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (valueToSearch == Numbers[i])
+                {
+                    Console.WriteLine($"{valueToSearch} was found in array!");
+                    return true;
+                }
+            }
+
+            Console.WriteLine($"{valueToSearch} was NOT found in array!");
+            return false;
+        }
+    }
+}
---
+++ 20241219_task8/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _20241219_task8
+{
+    internal class Program
+    {
+        /*
+         * Создайте интерфейс «IMath». В нём должно быть четыре метода: 
+
+        ■ int Max() — возвращает максимум; 
+        ■ int Min() — возвращает минимум;
+        ■ float Avg() — возвращает среднеарифметическое; 
+        ■ bool Search(int valueToSearch) — ищет valueSearch внутри контейнера данных.
+        Возвращает true, если значение найдено. Возвращает false, если значение не найдено.
+        
+        Класс, созданный в первом задании «Array», должен имплементировать интерфейс «IMath». 
+        
+        Метод Max — возвращает максимум среди элементов массива. 
+        Метод Min — возвращает минимум среди элементов массива. 
+        Метод Avg — возвращает среднеарифметическое среди элементов массива. 
+        Метод Search — ищет значение внутри массива. Возвращает true, если значение найдено. Возвращает false, если значение не найдено. Напишите код для тестирования полученной функциональности.
+         */
+        static void Main(string[] args)
+        {
+            ClassArray array = new ClassArray { Numbers = new int[]{ 10, 5, 2, 11, 18, 20, 70 } , Name = "My array"};
+            Console.WriteLine("\n-----------Original Array-----------\n");
+            PrintArray(array);
+
+            Console.WriteLine("\n\n-----------Max-----------\n");
+            Console.WriteLine(array.Max());
+
+            Console.WriteLine("\n-----------Min-----------\n");
+            Console.WriteLine(array.Min());
+
+            Console.WriteLine("\n-----------Avg-----------\n");
+            Console.WriteLine(array.Avg());
+
+            try
+            {
+                Console.WriteLine("\nEnter a number to search\n");
+                int valueToSearch = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"-----------Searching for {valueToSearch}-----------\n");
+                Console.WriteLine($"Result of searching opration: {array.Search(valueToSearch)}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void PrintArray(ClassArray array)
+        {
+            for (int i = 0; i < array.Numbers.Length; i++)
+            {
+                if (i == array.Numbers.Length - 1)
+                {
+                    Console.Write($"{array.Numbers[i]}");
+                }
+                else
+                {
+                    Console.Write($"{array.Numbers[i]} | ");
+                }
+                
+            }
+        }
+    }
+}
