3865725b1b98f3eb114d740dfa68bcdf1329cc45
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-19 13:25:08 -0700

    Practice interfaces in C#

---
+++ 20241219_task5/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241219_task5
+{
+    /*
+         Описать интерфейсы:
+
+«IResultShowInterface»: содержащий метод – Show. 
+«ISumInterface»: содержащий метод – Sum. 
+
+Используя интерфейсы, создать класс «Counter»  для расчета расчета электроэнергии по вводным показания и ее стоимости. Примерный вывод в консоль:
+
+Расчет электроэнергии
+
+Начальные показания : 568
+Конечные показания : 723
+Цена за 1кВт/ч : 1,68
+Сумма за электроэнергию : 260,40
+         */
+    internal class Counter
+    {
+        public double StartPoint { get; set; }
+        public double EndPoint { get; set; }
+        const double Price = 1.68;
+
+        public Counter(double start, double end)
+        {
+            StartPoint = start;
+            EndPoint = end;
+        }
+    }
+}
